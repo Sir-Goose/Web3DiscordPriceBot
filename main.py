942a6234044e837215ccc2d7dc00cce2319ca21e
Matthew <66563291+Sir-Goose@users.noreply.github.com>
2022-08-10 22:45:36 +0200

    Bot now replies to messages rather than replying in the channel.

--- main.py
+++ main.py
@@ -46,68 +46,70 @@ async def on_message(message):
         command = message.content
         tokenFormattedPrice = anyPrice.get_any_price(command, dict_from_csv)
 
-        await message.channel.send(tokenFormattedPrice)
+        await message.channel.send(tokenFormattedPrice, reference=message)
 
     if message.content.startswith('$h'):
         command = message.content
         historicalFormattedPrice = historicalPrice.get_historical_price(command, dict_from_csv)
-        await message.channel.send(historicalFormattedPrice)
+        await message.channel.send(historicalFormattedPrice, reference=message)
 
     if message.content.startswith('$convert'):
         command = message.content
         convertedValue = quote.fetchQuoute(command, dict_from_csv)
-        await message.channel.send(convertedValue)
+        await message.channel.send(convertedValue, reference=message)
 
-    if message.content.startswith('$tokens'):
-        trackedCoins = "```" + "imp\nimp\ngst\ngmt\ndei\ndeus\nboo\crv" + "```"
-        await message.channel.send(trackedCoins)
+    #if message.content.startswith('$tokens'):
+        #trackedCoins = "```" + "imp\nimp\ngst\ngmt\ndei\ndeus\nboo\crv" + "```"
+        #await message.channel.send(trackedCoins)
 
     if message.content.startswith('$request'):
         with open("requests.txt", "a") as external_file:
             add_text = message.content
             print(add_text, file=external_file)
             external_file.close()
-        await message.channel.send("**Received**")
+        await message.channel.send("**Received**", reference=message)
+        emoji = '\N{THUMBS UP SIGN}'
+        await message.add_reaction(emoji)
 
     if message.content.startswith('$imp'):
         command = message.content
         command = "$p imp"
         tokenFormattedPrice = anyPrice.get_any_price(command, dict_from_csv)
-        await message.channel.send(tokenFormattedPrice)
+        await message.channel.send(tokenFormattedPrice, reference=message)
 
     if message.content.startswith('$IMP'):
         command = message.content
         command = "$p imp"
         tokenFormattedPrice = anyPrice.get_any_price(command, dict_from_csv)
-        await message.channel.send(tokenFormattedPrice)
+        await message.channel.send(tokenFormattedPrice, reference=message)
 
     if message.content.startswith('$deus'):
         deusPrice = deiDeus.get_deus_price()
-        await message.channel.send(deusPrice)
+        await message.channel.send(deusPrice, reference=message)
 
     if message.content.startswith('$dei'):
         deiPrice = deiDeus.get_dei_price()
-        await message.channel.send(deiPrice)
+        await message.channel.send(deiPrice, reference=message)
 
     if message.content.startswith('$gst'):
         gstPrice = gstGmt.get_gst_price()
-        await message.channel.send(gstPrice)
+        await message.channel.send(gstPrice, reference=message)
 
     if message.content.startswith('$gmt'):
         gmtPrice = gstGmt.get_gmt_price()
-        await message.channel.send(gmtPrice)
+        await message.channel.send(gmtPrice, reference=message)
 
-    if message.content.startswith('$colortext'):
-        colortextexample = colorTest.color_test()
-        await message.channel.send(colortextexample)
+    #if message.content.startswith('$colortext'):
+        #colortextexample = colorTest.color_test()
+        #await message.channel.send(colortextexample, reference=message)
 
     if message.content.startswith('$boo'):
         booPrice = FtmEco.get_boo_price()
-        await message.channel.send(booPrice)
+        await message.channel.send(booPrice, reference=message)
 
     if message.content.startswith('$crv'):
         crvPrice = FtmEco.get_crv_price()
-        await message.channel.send(crvPrice)
+        await message.channel.send(crvPrice, reference=message)
 
     words = message.content
     print(words)
@@ -116,7 +118,7 @@ async def on_message(message):
     i = 0
     while i < length:
         if words[i] == 'meta':
-            await message.channel.send("Did you mean Facebook?")
+            await message.channel.send("Did you mean Facebook?", reference=message)
         i = i + 1
 
 client.run('')
